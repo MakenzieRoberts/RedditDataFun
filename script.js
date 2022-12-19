44ba2c57e7f923287a222f411ec1c69d3004c73f
MakenzieRoberts <makenzie.roberts@keyin.com>
2022-12-19 17:13:45 -0330

    More error handling + comments

--- script.js
+++ script.js
@@ -1,37 +1,87 @@
 const loader = document.querySelector("#loader");
-const content = document.querySelector("#bubble-chart");
+const chart = document.querySelector("#bubble-chart");
+const form = document.querySelector("#subreddit-input-form");
+const errorMsg = document.querySelector("#error-msg");
+const chartContainer = document.querySelector("#chart-container");
+const formButton = document.querySelector("#form-button");
 // Load-time Measurements
 // Before I make any performance improvements: Approx. 7579 - 9979 milliseconds
-// After locally-scoping unnecessary const variables:
+
+// Instead of sending the user input paramter through all those functions, just get the data returned from main and then send that to the chart function. way more readable and easier to understand. Do it in the promise function and rename it to
 
 addEventListener("DOMContentLoaded", async (event) => {
-	// Timer Reference: https://techblog.constantcontact.com/software-development/measure-page-load-times-using-the-user-timing-api/
+	form.addEventListener("submit", async (event) => {
+		event.preventDefault();
 
-	// Toggling the loading icon display, hiding it when the data is loaded.
-	loader.style.display = "block";
+		// Disabling the form button to prevent multiple submissions during loading.
+		formButton.disabled = true;
 
-	// Loading the data and timing how long it takes to load.
-	let startTime = window.performance.now();
-	const dataLoaded = await fetchData();
-	let endTime = window.performance.now();
+		// Hiding the loading icon, error message and chart if they are already displayed.
+		loader.style.display = "none";
+		chart.style.display = "none";
+		errorMsg.style.display = "none";
 
-	console.log(
-		"Loading the chart took " + (endTime - startTime) + " milliseconds."
-	);
+		// Getting the user input from the form.
+		const userInput = event.target[0].value;
 
-	loader.style.display = "none";
+		// Checking if the subreddit is available to fetch data from. If it is, the function returns the data, if not, it returns false.
+		const fetchedSubredditData = await handleSubredditFetch(userInput);
+
+		// Checking if the subreddit data is available or returned false due to invalid subreddit/error.
+		if (!fetchedSubredditData) {
+			// Displaying the error message if the subreddit doesn't exist.
+
+			errorMsg.style.display = "block";
+			// Re-enabling the form button so the user can enter another subreddit.
+			formButton.disabled = false;
+		} else {
+			// Toggling the loading icon display, hiding it when the data is loaded.
+			loader.style.display = "block";
+
+			// Awaiting promise from the function that calls the main logic function and sends the data to the chart.
+			const isLoaded = await loadChart(fetchedSubredditData);
+
+			// Hiding the loading icon and showing the chart, re-enabling the form button.
+			loader.style.display = "none";
+			chart.style.display = "block";
+			formButton.disabled = false;
+		}
+	});
 });
 
-function fetchData() {
+async function handleSubredditFetch(userInput) {
+	//      Description:   	Fetches the subreddit data based on the user input, catches errors and returns false if the subreddit is unavailable.
+	//      Parameter:     	userInput - The subreddit name entered by the user.
+	//      Returns:        Returns the subreddit data if the subreddit is available, otherwise returns false.
+	let subredditData = [];
+	try {
+		subredditData = await getSubredditData(userInput);
+		console.log(
+			"🚀 ~ file: script.js:30 ~ writeFrom.addEventListener ~ fetchTest",
+			subredditData
+		);
+		// If a subreddit is unavailable, the API returns an error object. If that is the case, throw an error.
+		if (subredditData.hasOwnProperty("error")) throw new Error();
+	} catch (error) {
+		return false;
+	}
+	return subredditData;
+}
+
+function loadChart(fetchedSubredditData) {
+	//      Description:   	Sends the data to the chart function and returns the data to the promise function.
+	//      Parameter:     	fetchedSubredditData - The data returned from fetching the subreddit json.
+	//      Returns:       	Resolves a promise when the chart is loaded.
 	return new Promise(async (resolve) => {
-		await bubbleChart();
+		const chartData = await main(fetchedSubredditData);
+		await bubbleChart(chartData);
 		resolve();
 	});
 }
 
-async function bubbleChart() {
+async function bubbleChart(chartData) {
+	const data = chartData;
 	// Here is where we call our logic functions that fetch and format the data behind the scenes.
-	const data = await main();
 
 	// Tutorial Credit: https://www.webtips.dev/how-to-make-interactive-bubble-charts-in-d3-js
 
@@ -49,14 +99,15 @@ async function bubbleChart() {
 	const generateChart = (data) => {
 		const bubble = (data) =>
 			d3.pack().size([width, height]).padding(2)(
-				d3.hierarchy({ children: data }).sum((d) => (d.score / 2) * 5)
+				d3.hierarchy({ children: data }).sum((d) => d.score)
 			);
 
 		const svg = d3
 			.select("#bubble-chart")
-			.style("width", width)
-			.style("height", height);
+			.attr("width", width) // Changed from .style to .attr for firefox compatibility
+			.attr("height", height); // Changed from .style to .attr for firefox compatibility
 
+		svg.selectAll("*").remove();
 		const root = bubble(data);
 		const tooltip = d3.select(".tooltip");
 
@@ -78,8 +129,8 @@ async function bubbleChart() {
 					.text(d.data.name);
 				tooltip
 					.select("span")
-					.attr("class", d.data.category)
-					.text(d.data.category);
+					.attr("class", "score")
+					.text("Users: " + d.data.score);
 				tooltip.style("visibility", "visible");
 
 				d3.select(this).style("stroke", "#222");
@@ -124,12 +175,15 @@ async function bubbleChart() {
 	generateChart(data);
 }
 
-async function main() {
-	// Get post data from r/ProgrammerHumor.json
-	const subredditData = await getSubredditData();
+async function main(fetchedSubredditData) {
+	/*
+	    Description:   	Main logic function that calls all other functions and returns the data to be used in the chart function.
+	    Parameter:     	fetchedSubredditData - The data returned from the main logic function.
+	    Returns:        The array of objects with no duplicates
+	*/
 
 	// Get unique authors from subreddit posts
-	const uniqueAuthors = getSubredditAuthors(subredditData);
+	const uniqueAuthors = getSubredditAuthors(fetchedSubredditData);
 
 	// Fetch user activity from each unique author's profile
 	const userActivity = await getUserActivity(uniqueAuthors);
@@ -152,14 +206,13 @@ async function main() {
 	hover.
 	*/
 	const dupesRemoved = removeDuplicates(formattedActivity);
-	console.log("calling dupesRemoved: ", dupesRemoved);
 
 	/*
 	Count and format data into an array of objects that can be used to create the bubble
-	chart.' Category is left empty at this time, but one day I intend to add a feature
-	that will utilize it. (In d3, category is used to color the bubbles, so I'm hoping in
-	the future I can find a way to color them based on the type of subreddit they are
-	(tech, gaming, memes, etc.) but I'm not sure where I'd get that data from yet.)
+	chart.' Category is left empty at this time, but one day (past the scope of this
+	sprint) I intend to add a feature that will utilize it. In d3, category is used to
+	color the bubbles, so I'm hoping in the future I can find a way to color them based on
+	the type of subreddit they are (tech, gaming, memes, etc.)
 
 		Format: {name: subreddit, category: "", score: count}
 	*/
@@ -169,79 +222,120 @@ async function main() {
 	We return the dataPoints array so that we can use it in the bubble chart function.
 	This function is called inside the bubble chart generation function.
 	*/
+
+	// Log table of data points to console
+	console.table(dataPoints);
+
 	return dataPoints;
 }
 
-async function getSubredditData() {
-	const data = await fetch("https://www.reddit.com/r/ProgrammerHumor.json")
-		.then((response) => response.json())
+async function getSubredditData(userInput) {
+	/*
+	    Description:   Fetch subreddit data from reddit.com/r/<userInput>.json
+	    Parameter:     The subreddit to fetch data from (user input)
+	    Returns:       Raw json data from the subreddit, or false if there is an error
+	 */
+
+	let data = [];
+	data = await fetch(`https://www.reddit.com/r/${userInput}.json`)
+		.then((response) => {
+			if (
+				response.hasOwnProperty("error") &&
+				!response.data.children.length > 0
+			) {
+				throw new Error();
+			}
+			return response.json();
+		})
 		.catch((error) => {
 			console.error("Error:", error);
+			return false;
 		});
-	// console.log(data);
-
+	console.log("🚀 ~ file: script.js:214 ~ getSubredditData ~ data", data);
 	return data;
 }
 
 function getSubredditAuthors(data) {
+	/*
+	     Description:   Gets authors from subreddit posts
+	     Parameter:     Raw subreddit data from r/<subreddit>.json
+	     Returns:       An array containing the unique authors from the subreddit
+	*/
+
 	const authors = data.data.children.map((post) => post.data.author);
-	// Only get unique authors
+
+	// Use set to only get unique authors
 	const uniqueAuthors = Array.from([...new Set(authors)]);
-	// console.log("unique authors", uniqueAuthors);
+
 	return uniqueAuthors;
 }
 
 // (?) Does this function need to be async? does the foreach need to be async?
 async function getUserActivity(uniqueAuthors) {
-	//      Description:    Fetch's user data and extracts the posts. Then creates a new
-	//                      object containing the username and their posts, and returns it.
-
-	//      Parameter:      uniqueAuthors - array of unique authors
-
-	//      Returns:         Sends back an object containing the user and their posts
-
-	// !IMPORTANT Make sure to only get subreddits where the user has posted, not comments. another function will handle comments (f1/f3)
+	/*
+		Description:    Fetch's user data and extracts the posts.
+		Parameter:      Array of unique authors
+		Returns:        An object containing the user and their posts, if a user's
+	     				account has been deleted or suspended, it will return false 
+						and is not included in the data.
+	*/
 
 	// For each post's author in the target subreddit, go to their profile and get their posts and extract the names of each subreddit they've posted in
-	// !IMPORTANT Make sure to only get subreddits where the user has posted, not comments. another function will handle comments
-	// !IMPORTANT Remember to put .CATCH statements in each fetch request!!
-	// !IMPORTANT I might be able to do multiple pages instead of getting just 25 results, we'll see. (also did user posts go from 26 ot 25? check how many posts are sent back)
 
-	const allAsyncResults = [];
+	let allActivityResults = [];
 
-	for (const item of uniqueAuthors) {
-		const asyncResult = await fetch(
-			`https://www.reddit.com/user/${item}.json`
-		).then((response) => response.json());
-		allAsyncResults.push({
-			user: item,
-			rawUserActivity: asyncResult.data.children,
-		});
+	for (const user of uniqueAuthors) {
+		let userActivityResult = [];
+		userActivityResult = await fetch(`https://www.reddit.com/user/${user}.json`)
+			.then((response) => {
+				return response.json();
+			})
+			.catch((error) => {
+				console.log(error);
+				console.log("Error fetching user activity for user: ", user);
+				return false;
+			});
+		/*
+		If an account has been deleted or suspended, the asyncResult will contain an
+		error property. We don't want to include those in our data, so we only push to
+		the array if there is no error property and the data is not empty or false.
+		*/
+		if (
+			!userActivityResult.hasOwnProperty("error") &&
+			userActivityResult.data.children.length > 0 &&
+			userActivityResult !== false
+		) {
+			allActivityResults.push({
+				user: user,
+				rawUserActivity: userActivityResult.data.children,
+			});
+		}
 	}
 
-	return allAsyncResults;
+	return allActivityResults;
 }
 
 function formatUserActivity(userActivity) {
 	/*
-	[ 
-		0:
-		{
-			user: "_unsusceptible",
-			rawUserActivity: [
-				{
-					kind: "t1",
-					data: {
-						subreddit_id: "t5_2tex6",
-					},
-				},
-			],
-		},
-	];
+	    Description:   	Formats user activity into an array of objects that can be
+	     				easily counted, while also removing NSFW posts from the data.
+	    Parameter:    	Array of objects containing user and their raw user activity
+		Example:
+						[ 0:
+							{
+								user: "_unsusceptible",
+								rawUserActivity: [
+									{
+										kind: "t1",
+										data: {
+											subreddit_id: "t5_2tex6",
+										},
+									},
+
+	    Returns:        An array of objects containing the subreddit, user, and type of post
 	*/
-	// This function will get subreddits that user has posted/commented in. Later I'll make the function that filters out one or the other using the t1/t3 values
+
 	let userSubreddits = [];
-	console.log({ userActivity });
 
 	userActivity.forEach((obj) => {
 		let user = obj.user;
@@ -260,12 +354,17 @@ function formatUserActivity(userActivity) {
 			userSubreddits.push(dataObj);
 		});
 	});
-	console.log("user subreddits", userSubreddits);
 
 	return userSubreddits;
 }
 
 function removeNSFW(obj) {
+	/*
+	    Description:   	Removes NSFW posts from an array of objects
+	    Parameter:      An object containing the user and their raw user activity
+	    Returns:        The object with NSFW posts removed
+	*/
+
 	const filteredData = obj.rawUserActivity.filter(checkNSFW);
 
 	function checkNSFW(post) {
@@ -277,26 +376,28 @@ function removeNSFW(obj) {
 }
 
 function removeDuplicates(arr) {
-	/**
-	 * Returns an array of objects with no duplicates
-	 * @param {Array} arr Array of Objects
-	 * @param {Array} keyProps Array of keys to determine uniqueness
-	 */
-	// Remove duplicates (I'm not interested in how much they've posted in each subreddit
-	// at this time, I just want to find out which subreddits they've posted/commented in)
+	/*
+	    Description:   	Removes duplicates from an array of objects
+	    Parameter:     	Array of objects containing the subreddit, user, and type of post
+	    Returns:        The array of objects with no duplicates
+	*/
 
-	// !IDEA: Later when counting I can individually count each comment/post and them add
-	// them together for the total. I can create a commentCount and postCount and use a
-	// simple if statement to add to each one depending on if type = t1/t3
+	/*
+	I'm not interested in how much they've posted in each subreddit at this time, I
+	just want to find out which subreddits they've posted/commented in)
 
-	// For the first stage of this project I'm not going to focus on whether or not the
-	// activity was a post or comment, so for now, if a user has both posted and commented
-	// in a subreddit, whichever is less recent will be removed as a duplicate. Although
-	// I'm not using the 'type' attribute yet, I'm going to keep it in the object for now,
-	// so later I can come back to this code and add the functionality to count posts and
-	// comments separately.
+	For the first stage of this project (within the scope of this sprint) I'm not going
+	to focus on whether or not the activity was a post or comment, so for now, if a
+	user has both posted and commented in a subreddit, whichever is less recent will be
+	removed as a duplicate.
 
-	// !REFERENCE: https://www.adamdehaven.com/blog/how-to-remove-duplicate-objects-from-a-javascript-array/#usage-example
+	Although I'm not using the 'type' attribute yet, I'm going to keep it in the object
+	for now, so that in the future I can come back to this code and add the
+	functionality to count posts and comments separately.
+	*/
+
+	// !REFERENCE: https://www.adamdehaven.com/blog/how-to-remove-duplicate-objects-from-a-javascript-array/#usage-
+
 	function getUniqueArray(arr, keyProps) {
 		return Object.values(
 			arr.reduce((uniqueMap, entry) => {
@@ -313,9 +414,15 @@ function removeDuplicates(arr) {
 }
 
 function formatDataPoint(dataArr) {
+	/*
+	    Description:   	Counts occurrences of each subreddit in the array of objects, then formats the data into an array of objects that can be used to create a chart
+	    Parameter:     	Array of objects containing the subreddit, user, and type of post
+	    Returns:        An array of objects that can be used to create the chart - containing the subreddit, category, and score
+	*/
 	// Counting Reference: https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements
 
 	let counts = {};
+
 	for (const data of dataArr) {
 		counts[data.subreddit] = counts[data.subreddit]
 			? counts[data.subreddit] + 1
@@ -333,7 +440,5 @@ function formatDataPoint(dataArr) {
 		dataPoints.push(singleDataPoint);
 	}
 
-	console.log("counts", counts);
-	console.log("dataPoints", dataPoints);
 	return dataPoints;
 }
